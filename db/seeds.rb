8b2c890aea2d9cd92b12d4e6d6b88b61e28532e2
ozubergz <jmin499@gmail.com>
2020-01-18 21:00:21 -0500

    second commit, add more artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 Artist.destroy_all
 Comment.destroy_all
@@ -14,7 +7,7 @@ Artist.create(
     name: "6ix9ine",
     profile_pic: "https://www.unilad.co.uk/wp-content/uploads/2018/07/tekashi69-wt.jpg", 
     bio: "Daniel Hernandez (born May 8, 1996), known professionally as 6ix9ine (pronounced 'six-nine') or Tekashi69 (also stylized as Tekashi 6ix9ine), is an American rapper, songwriter, and convicted felon. His music has been marked by an aggressive style of rapping, while his controversial public persona is characterized by his distinctive rainbow-colored hair, extensive tattoos, public feuds with fellow celebrities, and legal issues."
-);
+)
 
 Artist.create(
     name: "Bobby Shmurda",
@@ -39,6 +32,88 @@ Artist.create(
     profile_pic: "https://en.wikipedia.org/wiki/Kodak_Black#/media/File:Kodak_Black_Press_Photo_by_David_Cabrera.jpg",
     bio: "Bill K. Kapri (born Dieuson Octave; June 11, 1997), better known by his stage name Kodak Black, is an American rapper, singer and songwriter. He is noted for his singles 'Zeze', 'Roll in Peace', 'Tunnel Vision', and 'No Flockin', as well as his numerous legal issues."
 )
+
+Artist.create(
+    name: "21 Savage",
+    bio: "Shéyaa Bin Abraham-Joseph (born October 22, 1992), better known by his stage name 21 Savage, is a rapper, songwriter and record producer. He was born in London, England, and moved to Atlanta, Georgia when he was 7 years old.[8] He became known in Atlanta for the 2015 mixtape The Slaughter Tape before attaining nationwide attention following an EP collaboration Savage Mode (2016) with the producer Metro Boomin and its popular singles 'X' and 'No Heart', as well as a guest feature on the 2016 Drake single 'Sneakin'.",
+    profile_pic: "https://en.wikipedia.org/wiki/21_Savage#/media/File:21_Savage_2018.jpg"
+)
+
+Artist.create(
+    name: "XXXTentacion",
+    profile_pic: "https://en.wikipedia.org/wiki/XXXTentacion#/media/File:Xxxtentacion_mugshot.jpg",
+    bio: "Jahseh Dwayne Ricardo Onfroy (January 23, 1998 – June 18, 2018), known professionally as XXXTentacion was an American rapper, singer, songwriter and musician. Despite being a controversial figure due to myriad legal issues, Onfroy developed a substantial young following and bursts of popularity during his short career.[11] He was often credited by critics and fans for his musical-versatility, with his music primarily exploring trap, R&B, rock and emo."
+)
+
+Artist.create(
+    name: "Ski Mask the Slump God",
+    bio: "Stokeley Clevon Goulbourne (born April 18, 1996), known professionally as Ski Mask the Slump God (formerly stylized as $ki Mask 'The Slump God'), is an American rapper and songwriter. He originally collaborated with fellow American rapper XXXTentacion and their collective Members Only, and in 2017 released his songs 'Catch Me Outside' and 'Babywipe', which are both featured on his mixtape YouWillRegret and certified Gold by the RIAA.",
+    profile_pic: "https://en.wikipedia.org/wiki/Ski_Mask_the_Slump_God#/media/File:Ski_Mask_The_Slump_God_(2018).jpg"
+)
+
+Artist.create(
+    name: "Lil Pump",
+    bio: "Gazzy Garcia (born August 17, 2000), known professionally as Lil Pump, is an American rapper and songwriter. He is known for his hyperactive public persona, where he is often portrayed taking drugs, particularly marijuana, lean and Xanax; he is often criticized for his behavior seen on social media. He is also known for shouting his catchphrase 'Esskeetit' (a shortened version of 'Let's get it'). Garcia was named as one of the 30 Under 30 by Forbes in 2019.",
+    profile_pic: "https://en.wikipedia.org/wiki/Lil_Pump#/media/File:Lil_Pump_Icebox_2018_(cropped).jpg"
+)
+
+Artist.create(
+    name: "Smokepurpp",
+    bio: "Omar Jeffery Pineiro[6] (born May 15, 1997), known professionally as Smokepurpp, is an American rapper, singer, songwriter and record producer. Originally a record producer, Pineiro later found success on the audio distribution platform SoundCloud in 2016 after his song 'Audi' went viral along with his song 'smokepurpp on a bean'. Pineiro was part of the 'SoundCloud rap' movement, due to his influence on artists such as Lil Pump who have since entered the mainstream music market.",
+    profile_pic: "https://upload.wikimedia.org/wikipedia/commons/5/55/Smokepurpp.jpg"
+)
+
+Artist.create(
+    name: "Pop Smoke",
+    bio: "Bashar Barakah Jackson (born July 20, 1999), known professionally as Pop Smoke, is an American rapper and songwriter. He is signed to Republic Records. In April 2019 he released the song 'Welcome to the Party', the lead single of his debut mixtape Meet the Woo (2019) released in July 2019. 'Welcome to the Party' was made into two remixes featuring Nicki Minaj and Skepta in August 2019.[4] In October 2019 he featured American rapper Lil Tjay on his single 'War'. In December 2019 he collaborated with American rapper Travis Scott on the song, 'Gatti' from Scott and his Cactus Jack members' compilation album, JackBoys (2019).",
+    profile_pic: "https://www.rollingstone.com/wp-content/uploads/2020/01/pop-soke-arrested.jpg?resize=900,600&w=1200"
+)
+
+Artist.create(
+    name: "Travis Scott",
+    bio: "Jacques Berman Webster II (born April 30, 1992), known professionally as Travis Scott (formerly stylized as Travi$ Scott), is an American rapper, singer, songwriter, and record producer. In 2012, Scott signed his first major-label deal with Epic Records. In November of the same year, Scott signed a deal with Kanye West's GOOD Music, as part of its production wing Very GOOD Beats. In April 2013, Scott signed a record deal with T.I.'s Grand Hustle imprint.",
+    profile_pic: "https://upload.wikimedia.org/wikipedia/commons/e/e8/Travis_Scott_April_2017.jpg"
+)
+
+Artist.create(
+    name: "Gucci Mane",
+    bio: "Radric Delantic Davis (born February 12, 1980), known professionally as Gucci Mane, is an American rapper. He helped pioneer the hip hop subgenre of trap music alongside fellow Atlanta-based rappers T.I. and Young Jeezy, particularly in the 2000s and 2010s. In 2005, Gucci Mane debuted with Trap House, followed by his second and third albums, Hard to Kill and Trap-A-Thon in 2006. His fourth album, Back to the Trap House, was released in 2007.",
+    profile_pic: "https://upload.wikimedia.org/wikipedia/commons/7/71/Gucci_Mane_at_515_Alive_%28cropped_portrait%29.jpg"
+)
+
+Artist.create(
+    name: "Takeoff",
+    bio: "Kirshnik Khari Ball (born June 18, 1994), known professionally as Takeoff, is an American rapper, singer and songwriter. He is best known as a member of the hip hop and trap music trio Migos along with Quavo, his uncle, and Offset, his cousin.",
+    profile_pic: "https://upload.wikimedia.org/wikipedia/commons/6/6b/Takeoff_Veld_festival_2017.jpg"
+)
+
+Artist.create(
+    name: "Offset",
+    bio: "Kiari Kendrell Cephus (born December 14, 1991), known professionally as Offset, is an American rapper and songwriter. He is a member of the hip hop and trap music trio Migos, alongside his cousin Quavo and his nephew Takeoff. He is also an investor in esports organization FaZe Clan.",
+    profile_pic: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Offset_Veld_festival_2017.jpg"
+)
+
+Artist.create(
+    name: "Quavo",
+    bio: "Quavious Marshall (born April 2, 1991), known professionally as Quavo, is an American rapper, singer, composer and record producer. He is best known as a member of the hip hop and trap music trio Migos. Quavo is related to his fellow Migos members, being Takeoff's uncle and Offset's cousin.",
+    profile_pic: "https://en.wikipedia.org/wiki/Quavo#/media/File:Quavo_Veld_festival_2017.jpg"
+)
+Artist.create(
+    name: "Lil Wayne",
+    bio: "Dwayne Michael Carter Jr. (born September 27, 1982), known professionally as Lil Wayne, is an American rapper, singer, songwriter, record executive, entrepreneur, and actor. His career began in 1996, at the age of 13, when he was discovered by Birdman and joined Cash Money Records as the youngest member of the label. He joined the southern hip hop group Hot Boys, with Cash Money label-mates Juvenile and Turk, in 1997, who became popular following the release of the album Guerrilla Warfare (1999) and the 'Bling Bling'. For many years, Lil Wayne was the flagship artist of Cash Money Records, before ending his long-tenured deal with the company in June 2018.",
+    profile_pic: "https://upload.wikimedia.org/wikipedia/commons/a/a6/Lil_Wayne_%2823513397583%29.jpg"
+)
+Artist.create(
+    name: "Drake",
+    bio: "Aubrey Drake Graham (born October 24, 1986) is a Canadian rapper, singer, songwriter, producer, actor, and businessman. Drake initially gained recognition as an actor on the teen drama television series Degrassi: The Next Generation in the 2000s; intent on pursuing a career in music, he left the series in 2007 after releasing his debut mixtape Room for Improvement. He released two further independent projects, Comeback Season and So Far Gone, before signing to Young Money Entertainment in June 2009.",
+    profile_pic: "https://upload.wikimedia.org/wikipedia/commons/8/81/Drake_at_the_Velvet_Underground_-_2017_%2835986086223%29_%28cropped%29.jpg"
+)
+Artist.create(
+    name: "Cardi B",
+    bio: "Belcalis Marlenis Almánzar (born October 11, 1992), known professionally as Cardi B, is an American rapper, songwriter and television personality. Born in Manhattan and raised in The Bronx, New York City, she became an internet celebrity after several of her posts and videos became viral on Vine and Instagram. From 2015 to 2017, she appeared as a regular cast member on the VH1 reality television series Love & Hip Hop: New York to follow her music aspirations, and released two mixtapes—Gangsta Bitch Music, Vol. 1 and Vol. 2, before signing with label Atlantic Records in early 2017.",
+    profile_pic: "https://thefederalist.com/wp-content/uploads/2020/01/Cardi-B.jpg"
+)
+
 
 10.times do
     Comment.create(
